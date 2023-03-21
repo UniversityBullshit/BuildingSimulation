dd9bd25427bd260cb97780a75b88b66561327976
cheremshin <sergey.cheremshin@gmail.com>
2023-03-22 06:03:01 +0700

    feat: Add GetBuildings method and constructor. All fields are private

--- src/main/java/com/universitybullshit/model/Habitat.java
+++ src/main/java/com/universitybullshit/model/Habitat.java
@@ -1,14 +1,22 @@
 package com.universitybullshit.model;
 
-import com.universitybullshit.model.Building;
-
 import java.util.List;
 
 public class Habitat {
-    int width;
-    int height;
-    List<Building> buildings;
-    public void Update(long time) {
+    private int width;
+    private int height;
+    private long time;
+    private List<Building> buildings;
+    public Habitat(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.time = 0;
+        this.buildings = null;
+    }
+    private void Update(long time) {
         // Генерация новых объектов и размещение в поле
+    }
+    public List<Building> GetBuildigs() {
+        return this.buildings;
     }
 }
