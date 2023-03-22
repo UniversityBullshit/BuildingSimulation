bb6daa81d1703db50f0e81c229c602aacd30b4d6
cheremshin <sergey.cheremshin@gmail.com>
2023-03-22 10:40:50 +0700

    feat: Set static constants, implement all needed methods

--- src/main/java/com/universitybullshit/model/CapitalBuilding.java
+++ src/main/java/com/universitybullshit/model/CapitalBuilding.java
@@ -1,4 +1,33 @@
 package com.universitybullshit.model;
 
-public class CapitalBuilding {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CapitalBuilding extends Building {
+    private static final long interval = 3000;
+    private static final double probability = 0.7;
+    private static final int size = 2;
+    private int x;
+    private int y;
+
+    public CapitalBuilding(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public static long GetInterval() {
+        return interval;
+    }
+    public static double GetProbability() {
+        return probability;
+    }
+    public static int GetSize() {
+        return size;
+    }
+    @Override
+    public ArrayList<Integer> GetCoordinates() {
+        ArrayList<Integer> coordinates = new ArrayList<Integer>();
+        coordinates.add(this.x);
+        coordinates.add(this.y);
+        return coordinates;
+    }
 }
--- src/main/java/com/universitybullshit/model/WoodenBuilding.java
+++ src/main/java/com/universitybullshit/model/WoodenBuilding.java
@@ -1,5 +1,32 @@
 package com.universitybullshit.model;
 
-public class WoodenBuilding {
+import java.util.ArrayList;
 
+public class WoodenBuilding extends Building {
+    private static final long interval = 2000;
+    private static final double probability = 0.3;
+    private static final int size = 2;
+    private int x;
+    private int y;
+
+    public WoodenBuilding(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public static long GetInterval() {
+        return interval;
+    }
+    public static double GetProbability() {
+        return probability;
+    }
+    public static int GetSize() {
+        return size;
+    }
+    @Override
+    public ArrayList<Integer> GetCoordinates() {
+        ArrayList<Integer> coordinates = new ArrayList<Integer>();
+        coordinates.add(this.x);
+        coordinates.add(this.y);
+        return coordinates;
+    }
 }
