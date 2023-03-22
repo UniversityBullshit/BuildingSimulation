c6c1ef3dbc36e876a838a402c307da64d6cdd511
cheremshin <sergey.cheremshin@gmail.com>
2023-03-22 10:41:42 +0700

    feat: Add base logic to Update method

--- src/main/java/com/universitybullshit/model/Habitat.java
+++ src/main/java/com/universitybullshit/model/Habitat.java
@@ -1,6 +1,7 @@
 package com.universitybullshit.model;
 
 import java.util.List;
+import java.util.Random;
 
 public class Habitat {
     private int width;
@@ -10,13 +11,31 @@ public class Habitat {
     public Habitat(int width, int height) {
         this.width = width;
         this.height = height;
-        this.time = 0;
+        this.time = System.currentTimeMillis();
         this.buildings = null;
     }
-    private void Update(long time) {
+    public void Update(long time) {
         // Генерация новых объектов и размещение в поле
+        Random generator = new Random();
+        double chance = generator.nextDouble();
+
+        if (chance > CapitalBuilding.GetProbability()) {
+            if (time - this.time > CapitalBuilding.GetInterval()) {
+                System.out.println("Ищем место, где можно разместить CapitalBuilding и ставим");
+                this.time = time;
+            }
+        }
+
+        if (chance > WoodenBuilding.GetProbability()) {
+            if (time - this.time > WoodenBuilding.GetInterval()) {
+                System.out.println("Ищем место, где можно разместить WoodenBuilding и ставим");
+                this.time = time;
+            }
+        }
+
+        System.out.printf("Diff: %d\n", time - this.time);
     }
-    public List<Building> GetBuildigs() {
+    public List<Building> GetBuildings() {
         return this.buildings;
     }
 }
