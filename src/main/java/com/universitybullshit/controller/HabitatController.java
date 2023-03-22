627879c1b113e4ee45d0d935fbce09239ad8f87a
cheremshin <sergey.cheremshin@gmail.com>
2023-03-22 10:42:34 +0700

    feat: Some refactoring done, complete most methods

--- src/main/java/com/universitybullshit/controller/HabitatController.java
+++ src/main/java/com/universitybullshit/controller/HabitatController.java
@@ -1,17 +1,39 @@
 package com.universitybullshit.controller;
 
+import com.universitybullshit.model.Building;
 import com.universitybullshit.model.Habitat;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.List;
+
 public class HabitatController {
-    private boolean isSimulationStarted = false;
+    private boolean isSimulationRunning = false;
     private long time;
-    public void StartSimulation() {
-        Habitat habitat = new Habitat(800, 600);
+    private Habitat context;
+    public HabitatController(Habitat context) {
+        this.context = context;
+        this.time = System.currentTimeMillis();
     }
-    public void StartSimulation(int width, int height) {
-        Habitat habitat = new Habitat(width, height);
+    public void StartSimulation() {
+        this.isSimulationRunning = true;
+
+        CompletableFuture.runAsync(() -> Simulation());
     }
     public void StopSimulation() {
-
+        this.isSimulationRunning = false;
+    }
+    public List<Building> GetBuildings() {
+        return this.context.GetBuildings();
+    }
+    private void Simulation() {
+        while (this.isSimulationRunning) {
+            this.context.Update(this.time);
+            this.time = System.currentTimeMillis();
+            try {
+                Thread.sleep(1000); // 25 updates per second
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }
