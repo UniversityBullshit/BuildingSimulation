9769a318fc80cdf4db8691d426353b4e548445a4
Oleg Asimov <funtazoid@yandex.ru>
2023-03-25 03:00:27 +0700

    ref: added annotation for method

--- src/main/java/com/universitybullshit/view/actions/AboutMenuItemListener.java
+++ src/main/java/com/universitybullshit/view/actions/AboutMenuItemListener.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AboutMenuItemListener implements ActionListener {
+    @Override
     public void actionPerformed(ActionEvent e) {
         JDialog aboutDialog = AboutFrame.create(MainFrame.getCtx());
         aboutDialog.setVisible(true);
