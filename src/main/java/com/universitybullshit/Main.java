45f9e91d993fcc9c57f8f36890e342faf20a4f92
cheremshin <sergey.cheremshin@gmail.com>
2023-03-25 02:14:22 +0700

    feat: Change application building logic

--- src/main/java/com/universitybullshit/Main.java
+++ src/main/java/com/universitybullshit/Main.java
@@ -1,8 +1,17 @@
 package com.universitybullshit;
 
+import com.universitybullshit.controller.HabitatController;
+import com.universitybullshit.model.Habitat;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
+
 import com.universitybullshit.view.MainFrame;
 public class Main {
     public static void main(String[] args) {
+        HabitatController habitatController = new HabitatController(800, 600);
+        habitatController.StartSimulation();
         MainFrame.createFrame();
+        ForkJoinPool.commonPool().awaitQuiescence(5, TimeUnit.SECONDS);
     }
 }
