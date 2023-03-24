aefd0b32cf6ca311326bca848c601249d4f12f34
Oleg Asimov <funtazoid@yandex.ru>
2023-03-25 03:03:15 +0700

    ref: Main class must only create a window

--- src/main/java/com/universitybullshit/Main.java
+++ src/main/java/com/universitybullshit/Main.java
@@ -1,7 +1,7 @@
 package com.universitybullshit;
 
-import com.universitybullshit.controller.HabitatController;
-import com.universitybullshit.model.Habitat;
+//import com.universitybullshit.controller.HabitatController;
+//import com.universitybullshit.model.Habitat;
 
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
@@ -9,8 +9,8 @@ import java.util.concurrent.TimeUnit;
 import com.universitybullshit.view.MainFrame;
 public class Main {
     public static void main(String[] args) {
-        HabitatController habitatController = new HabitatController(800, 600);
-        habitatController.StartSimulation();
+//        HabitatController habitatController = new HabitatController(800, 600);
+//        habitatController.StartSimulation();
         MainFrame.createFrame();
         ForkJoinPool.commonPool().awaitQuiescence(5, TimeUnit.SECONDS);
     }
