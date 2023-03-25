9b2014a1ff34189bb3acc19cf41aac3b285889d1
cheremshin <sergey.cheremshin@gmail.com>
2023-03-25 19:22:22 +0700

    feat: Buildings field is Vector now. Add ids HashSet and spawnTimeMap TreeMap. Lombok getters. Public DeleteExpiredObjects method.

--- src/main/java/com/universitybullshit/model/Habitat.java
+++ src/main/java/com/universitybullshit/model/Habitat.java
@@ -1,8 +1,12 @@
 package com.universitybullshit.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import lombok.Getter;
+
+import java.util.HashSet;
+import java.util.TreeMap;
+import java.util.Vector;
 import java.util.Random;
+import java.util.Iterator;
 
 public class Habitat {
     private final int width;
@@ -10,66 +14,92 @@ public class Habitat {
     private long time;
     private long lastWoodenBuildingSpawnTime;
     private long lastCapitalBuildingSpawnTime;
-    private final List<Building> buildings;
+    @Getter
+    private final Vector<Building> buildings;
+    @Getter
+    private final HashSet<Long> ids;
+    @Getter
+    private final TreeMap<Long, Long> spawnTimeMap;
+    @Getter
     private int woodenBuildingsCount;
+    @Getter
     private int capitalBuildingsCount;
     public Habitat(int width, int height) {
         this.width = width;
         this.height = height;
-        this.time = System.currentTimeMillis();
-        this.lastWoodenBuildingSpawnTime = this.time;
-        this.lastCapitalBuildingSpawnTime = this.time;
-        this.buildings = new ArrayList<>();
+        this.time = 0;
+        this.lastWoodenBuildingSpawnTime = 0;
+        this.lastCapitalBuildingSpawnTime = 0;
+        this.buildings = new Vector<>();
+        this.ids = new HashSet<>();
+        this.spawnTimeMap = new TreeMap<>();
         this.woodenBuildingsCount = 0;
         this.capitalBuildingsCount = 0;
     }
     public void Update(long time) {
-        SpawnCapitalBuilding(time);
-        SpawnWoodenBuilding(time);
-        System.out.println(time);
         this.time = time;
+        DeleteExpiredObjects();
+        SpawnCapitalBuilding();
+        SpawnWoodenBuilding();
     }
-    private void SpawnWoodenBuilding(long time) {
+    private void SpawnWoodenBuilding() {
         Random generator = new Random();
 
         double chance = generator.nextDouble();
-        if (chance > WoodenBuilding.GetProbability()) {
-            if (time - this.lastWoodenBuildingSpawnTime > WoodenBuilding.GetInterval()) {
+        if (chance > WoodenBuilding.getProbability()) {
+            if (this.time - this.lastWoodenBuildingSpawnTime >= WoodenBuilding.getInterval()) {
                 int x = generator.nextInt(this.width);
                 int y = generator.nextInt(this.height);
 
-                WoodenBuilding woodenBuilding = new WoodenBuilding(x, y);
+                WoodenBuilding woodenBuilding = new WoodenBuilding(x, y, this.time);
 
                 this.buildings.add(woodenBuilding);
+                this.ids.add(woodenBuilding.getId());
+                this.spawnTimeMap.put(woodenBuilding.getId(), this.time);
                 this.woodenBuildingsCount++;
-                this.lastWoodenBuildingSpawnTime = time;
+                this.lastWoodenBuildingSpawnTime = this.time;
             }
         }
     }
-    private void SpawnCapitalBuilding(long time) {
+    private void SpawnCapitalBuilding() {
         Random generator = new Random();
 
         double chance = generator.nextDouble();
-        if (chance > CapitalBuilding.GetProbability()) {
-            if (time - this.lastCapitalBuildingSpawnTime > CapitalBuilding.GetInterval()) {
+        if (chance > CapitalBuilding.getProbability()) {
+            if (this.time - this.lastCapitalBuildingSpawnTime >= CapitalBuilding.getInterval()) {
                 int x = generator.nextInt(this.width);
                 int y = generator.nextInt(this.height);
 
-                CapitalBuilding capitalBuilding = new CapitalBuilding(x, y);
+                CapitalBuilding capitalBuilding = new CapitalBuilding(x, y, this.time);
 
                 this.buildings.add(capitalBuilding);
+                this.ids.add(capitalBuilding.getId());
+                this.spawnTimeMap.put(capitalBuilding.getId(), this.time);
                 this.capitalBuildingsCount++;
-                this.lastCapitalBuildingSpawnTime = time;
+                this.lastCapitalBuildingSpawnTime = this.time;
             }
         }
     }
-    public List<Building> GetBuildings() {
-        return this.buildings;
+    public void DeleteExpiredObjects() {
+        for (Building building : this.buildings) {
+            boolean shouldDelete = false;
+            if (building instanceof WoodenBuilding) {
+                if (this.time - building.getSpawnTime() >= WoodenBuilding.getLifeTime()) {
+                    shouldDelete = true;
+                }
+            } else {
+                if (this.time - building.getSpawnTime() >= CapitalBuilding.getLifeTime()) {
+                    shouldDelete = true;
+                }
+            }
+            if (shouldDelete) {
+                RemoveObject(building);
+            }
+        }
     }
-    public int GetWoodenBuildingsCount() {
-        return this.woodenBuildingsCount;
-    }
-    public int GetCapitalBuildingsCount() {
-        return this.capitalBuildingsCount;
+    private void RemoveObject(Building building) {
+        this.ids.remove(building.getId());
+        this.spawnTimeMap.remove(building.getId());
+        this.buildings.remove(building);
     }
 }
