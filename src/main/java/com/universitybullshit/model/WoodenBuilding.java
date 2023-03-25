9ed32558ae9d38def0c1f84f7cb6505f70ea45ba
cheremshin <sergey.cheremshin@gmail.com>
2023-03-25 19:19:00 +0700

    feat: Add id, spawnTime and lifeTime fields, lombok getters, SetDefaults method

--- src/main/java/com/universitybullshit/model/WoodenBuilding.java
+++ src/main/java/com/universitybullshit/model/WoodenBuilding.java
@@ -1,22 +1,33 @@
 package com.universitybullshit.model;
 
+import lombok.Getter;
+
 import java.util.ArrayList;
 
 public class WoodenBuilding extends Building {
-    private static final long interval = 2000;
-    private static final double probability = 0.3;
+    @Getter
+    private long id;
+    @Getter
+    private static long interval;
+    @Getter
+    private static double probability;
     private int x;
     private int y;
+    @Getter
+    private long spawnTime;
+    @Getter
+    private static long lifeTime;
 
-    public WoodenBuilding(int x, int y) {
+    public WoodenBuilding(int x, int y, long time) {
         this.x = x;
         this.y = y;
+        this.spawnTime = time;
+        this.id = id;
     }
-    public static long GetInterval() {
-        return interval;
-    }
-    public static double GetProbability() {
-        return probability;
+    public static void SetDefaults(long interval, double probability, long lifeTime) {
+        WoodenBuilding.interval = interval;
+        WoodenBuilding.probability = probability;
+        WoodenBuilding.lifeTime = lifeTime;
     }
     @Override
     public ArrayList<Integer> GetCoordinates() {
