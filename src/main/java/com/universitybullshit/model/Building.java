936c3ebe4d370a6404eb6b865b0c4a46c795944c
cheremshin <sergey.cheremshin@gmail.com>
2023-03-25 19:17:06 +0700

    feat: Add lombok getters to have abstract access

--- src/main/java/com/universitybullshit/model/Building.java
+++ src/main/java/com/universitybullshit/model/Building.java
@@ -1,12 +1,22 @@
 package com.universitybullshit.model;
+import lombok.Getter;
+
 import java.lang.Integer;
 import java.util.ArrayList;
 
 public abstract class Building {
+    @Getter
+    private long id;
+    @Getter
     private static long interval;
+    @Getter
     private static double probability;
     private int x;
     private int y;
+    @Getter
+    private long spawnTime;
+    @Getter
+    private static long lifeTime;
 
     public abstract ArrayList<Integer> GetCoordinates();
 }
