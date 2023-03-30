99ad1d3d6a92c15101b791986ec4d114a3d01c2b
cheremshin <sergey.cheremshin@gmail.com>
2023-03-31 02:24:01 +0700

    feat: Change update and painting logic

--- src/main/java/com/universitybullshit/view/Area.java
+++ src/main/java/com/universitybullshit/view/Area.java
@@ -4,12 +4,16 @@ import com.universitybullshit.controller.HabitatController;
 import com.universitybullshit.model.CapitalBuilding;
 import com.universitybullshit.view.component.ImageFactory;
 import com.universitybullshit.model.Building;
+import com.universitybullshit.view.util.BuildingDrawDto;
+import lombok.Getter;
 import lombok.Setter;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Vector;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 
 public class Area extends JPanel {
@@ -19,9 +23,12 @@ public class Area extends JPanel {
     private final JPanel root;
     private final ImageFactory imageFactory = new ImageFactory();
     private final HabitatController controller;
+    @Getter
     @Setter
     private boolean isAreaUpdating = false;
-//    private Vector<Building> buildingsCache = new Vector<>();
+    public final HashMap<Long, BuildingInstance> buildingsDictionary = new HashMap<>();
+    private final Vector<Long> deletingBuildings = new Vector<>();
+    private final Vector<Building> addingBuildings = new Vector<>();
 
     public Area(int width, int height, JPanel root, HabitatController controller) {
         this.width = width;
@@ -41,38 +48,84 @@ public class Area extends JPanel {
     }
 
     public void update() {
-        while (isAreaUpdating) {
-            spawnBuilding();
-            this.updateUI();
-            try {
-                Thread.sleep(50);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
+        // Get actual info
+        getDeletingInstances();
+        getAddingInstances();
+
+        // Accept changes
+        removeInstances();
+        addInstances();
+        //moveInstances();
+
+        // Delete temporary info
+        this.deletingBuildings.clear();
+        this.addingBuildings.clear();
+
+        repaint();
+    }
+
+    private void getDeletingInstances() {
+        HashSet<Long> actualBuildingsIds = this.controller.getIds();
+        for (Long id : this.buildingsDictionary.keySet()) {
+            if (!actualBuildingsIds.contains(id)) {
+                this.deletingBuildings.add(id);
             }
         }
     }
 
-    private void spawnBuilding() {
-        Vector<Building> buildings = this.controller.getBuildings();
-        System.out.println(buildings.size());
-        for (Building building : buildings) {
-            ArrayList<Integer> coords = building.getCoordinates();
-            BuildingInstance buildingInstance = new BuildingInstance(building instanceof CapitalBuilding, coords.get(0), coords.get(1));
-            this.add(buildingInstance);
-            this.remove(buildingInstance);
+    private void getAddingInstances() {
+        Vector<Building> actualBuildings = this.controller.getBuildings();
+        for (Building building : new CopyOnWriteArrayList<>(actualBuildings)) {
+            if (!this.buildingsDictionary.containsKey(building.getId())) {
+                this.addingBuildings.add(building);
+            }
         }
-//        Vector<Building> buildings = this.controller.getBuildings();
-//        if (this.buildingsCache.isEmpty() && !buildings.isEmpty()) {
-//            this.buildingsCache = buildings;
-//            for (Building building : this.buildingsCache) {
-//                ArrayList<Integer> coords = building.getCoordinates();
-//                BuildingInstance buildingInstance = new BuildingInstance(building instanceof CapitalBuilding,
-//                        coords.get(0),
-//                        coords.get(1));
-//                this.add(buildingInstance);
-//            }
-//        } else {
-//            System.out.printf("%d %d", buildings, buildingsCache);
-//        }
+    }
+
+    private void removeInstances() {
+        for (Long id : this.deletingBuildings) {
+            this.buildingsDictionary.remove(id);
+        }
+    }
+
+    private void addInstances() {
+        for (Building building : this.addingBuildings) {
+            this.buildingsDictionary.put(building.getId(), new BuildingInstance(building));
+        }
+    }
+
+    private void moveInstances() {
+        Vector<Building> actualBuildings = this.controller.getBuildings();
+        for (Building building : new CopyOnWriteArrayList<>(actualBuildings)) {
+            if (!this.addingBuildings.contains(building)) {
+                Long id = building.getId();
+                int currentX = this.buildingsDictionary.get(id).getX();
+                int currentY = this.buildingsDictionary.get(id).getY();
+                if (isDifferentCoordinates(currentX, currentY, building.getX(), building.getY())) {
+                    this.buildingsDictionary.get(id).setX(currentX);
+                    this.buildingsDictionary.get(id).setY(currentY);
+                }
+            }
+        }
+    }
+
+    private boolean isDifferentCoordinates(int currentX, int currentY, int actualX, int actualY) {
+        return currentX != actualX || currentY != actualY;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for (BuildingInstance buildingInstance : this.buildingsDictionary.values()) {
+            if (buildingInstance.isCapitalBuilding()) {
+                Integer side = BuildingDrawDto.getCapitalBuildingSide();
+                g.setColor(BuildingDrawDto.getCapitalBuildingColor());
+                g.fillRect(buildingInstance.getX(), buildingInstance.getY(), side, side);
+            } else if (buildingInstance.isWoodenBuilding()) {
+                Integer side = BuildingDrawDto.getWoodenBuildingSide();
+                g.setColor(BuildingDrawDto.getWoodenBuildingColor());
+                g.fillRect(buildingInstance.getX(), buildingInstance.getY(), side, side);
+            }
+        }
     }
 }
