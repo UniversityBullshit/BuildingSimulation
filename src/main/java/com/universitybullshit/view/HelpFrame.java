ae916603303db96d4528992089360040813dc018
cheremshin <sergey.cheremshin@gmail.com>
2023-03-31 16:31:43 +0700

    feat: Change labels positioning

--- src/main/java/com/universitybullshit/view/HelpFrame.java
+++ src/main/java/com/universitybullshit/view/HelpFrame.java
@@ -1,18 +1,19 @@
 package com.universitybullshit.view;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
-public class AboutFrame {
+public class HelpFrame {
     private static final int width = 240;
     private static final int height = 120;
-    private static final String title = "About";
-    public static JDialog create(JFrame frame) {
+    public static JDialog create(JFrame frame, String title) {
         JDialog dialog = new JDialog(frame, title, true);
         JPanel panel = new JPanel();
         dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         dialog.setMinimumSize(new Dimension(width, height));
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setBorder(new EmptyBorder(new Insets(10, 10, 0, 0)));
         dialog.setLocationRelativeTo(null);
         dialog.setResizable(false);
         dialog.add(panel);
