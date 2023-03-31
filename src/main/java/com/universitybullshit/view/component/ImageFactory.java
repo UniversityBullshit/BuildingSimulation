c5ae2dc7801d22fb8ff968f1d6fb5f004de35d38
Oleg Asimov <funtazoid@yandex.ru>
2023-03-31 17:31:32 +0700

    refactor: change file path

--- src/main/java/com/universitybullshit/view/component/ImageFactory.java
+++ src/main/java/com/universitybullshit/view/component/ImageFactory.java
@@ -5,23 +5,24 @@ import lombok.Getter;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.net.URL;
 
 public class ImageFactory {
-    private static final String WOODEN_BUILDING_ICON_PATH = "./src/main/resources/woodenBuilding.png";
-    private static final String CAPITAL_BUILDING_ICON_PATH = "./src/main/resources/capitalBuilding.png";
-    private static final String IT_DEPRECATED_PATH = "./src/main/resources/it_deprecated.png";
+//    private static final String WOODEN_BUILDING_ICON_PATH = "./src/main/resources/woodenBuilding.png";
+//    private static final String CAPITAL_BUILDING_ICON_PATH = "./src/main/resources/capitalBuilding.png";
+    private static final URL IT_DEPRECATED_PATH = ImageFactory.class.getResource("/it_deprecated.png");
 
-    @Getter
-    private final Image woodenBuilding;
-    @Getter
-    private final Image capitalBuilding;
+//    @Getter
+//    private final Image woodenBuilding;
+//    @Getter
+//    private final Image capitalBuilding;
 
     @Getter
     private final Image itDeprecated;
 
     public ImageFactory() {
-        this.woodenBuilding = new ImageIcon(WOODEN_BUILDING_ICON_PATH).getImage();
-        this.capitalBuilding = new ImageIcon(CAPITAL_BUILDING_ICON_PATH).getImage();
+//        this.woodenBuilding = new ImageIcon(WOODEN_BUILDING_ICON_PATH).getImage();
+//        this.capitalBuilding = new ImageIcon(CAPITAL_BUILDING_ICON_PATH).getImage();
         this.itDeprecated = new ImageIcon(IT_DEPRECATED_PATH).getImage();
     }
 }
