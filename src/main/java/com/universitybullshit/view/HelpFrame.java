4ac6d39ee75512b3d6dc604b979d2947560be747
Oleg Asimov <funtazoid@yandex.ru>
2023-03-31 16:57:57 +0700

    feat: add logo

--- src/main/java/com/universitybullshit/view/HelpFrame.java
+++ src/main/java/com/universitybullshit/view/HelpFrame.java
@@ -1,22 +1,31 @@
 package com.universitybullshit.view;
 
+import com.universitybullshit.view.component.ImageFactory;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class HelpFrame {
     private static final int width = 240;
-    private static final int height = 120;
+    private static final int height = 180;
+    private static final ImageFactory imageFactory = new ImageFactory();
+
     public static JDialog create(JFrame frame, String title) {
         JDialog dialog = new JDialog(frame, title, true);
+        JPanel imgPanel = new JPanel();
         JPanel panel = new JPanel();
+        dialog.setLayout(new BorderLayout());
         dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         dialog.setMinimumSize(new Dimension(width, height));
+        imgPanel.setBorder(new EmptyBorder(new Insets(15, 0, 0, 0)));
+        imgPanel.add(new JLabel(new ImageIcon(imageFactory.getItDeprecated().getScaledInstance(115, 42, Image.SCALE_SMOOTH))));
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.setBorder(new EmptyBorder(new Insets(10, 10, 0, 0)));
         dialog.setLocationRelativeTo(null);
         dialog.setResizable(false);
-        dialog.add(panel);
+        dialog.add(imgPanel, BorderLayout.NORTH);
+        dialog.add(panel, BorderLayout.CENTER);
         panel.add(new JLabel("BuildingSpawner"));
         panel.add(new JLabel("Version: 1.0-SNAPSHOT"));
         panel.add(new JLabel("Authors: Sergei Cheremisin, Arounte"));
--- src/main/java/com/universitybullshit/view/component/ImageFactory.java
+++ src/main/java/com/universitybullshit/view/component/ImageFactory.java
@@ -4,18 +4,24 @@ import lombok.Getter;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class ImageFactory {
     private static final String WOODEN_BUILDING_ICON_PATH = "./src/main/resources/woodenBuilding.png";
     private static final String CAPITAL_BUILDING_ICON_PATH = "./src/main/resources/capitalBuilding.png";
+    private static final String IT_DEPRECATED_PATH = "./src/main/resources/it_deprecated.png";
 
     @Getter
     private final Image woodenBuilding;
     @Getter
     private final Image capitalBuilding;
 
+    @Getter
+    private final Image itDeprecated;
+
     public ImageFactory() {
         this.woodenBuilding = new ImageIcon(WOODEN_BUILDING_ICON_PATH).getImage();
         this.capitalBuilding = new ImageIcon(CAPITAL_BUILDING_ICON_PATH).getImage();
+        this.itDeprecated = new ImageIcon(IT_DEPRECATED_PATH).getImage();
     }
 }
