0fe8de80d915cb34b64cc0e7603d36749e8514f0
cheremshin <sergey.cheremshin@gmail.com>
2023-04-07 23:42:06 +0700

    refactor: Remove unused imports

--- src/main/java/com/universitybullshit/Main.java
+++ src/main/java/com/universitybullshit/Main.java
@@ -1,8 +1,5 @@
 package com.universitybullshit;
 
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.TimeUnit;
-
 import com.universitybullshit.view.MainFrame;
 public class Main {
     public static void main(String[] args) {
