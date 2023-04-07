fc5c9058887c2f41b4ab9699c5857a24fd08b4a9
cheremshin <sergey.cheremshin@gmail.com>
2023-04-07 23:41:40 +0700

    refactor: Remove unused ForkJoinPool await

--- src/main/java/com/universitybullshit/Main.java
+++ src/main/java/com/universitybullshit/Main.java
@@ -7,6 +7,5 @@ import com.universitybullshit.view.MainFrame;
 public class Main {
     public static void main(String[] args) {
         MainFrame.mainMenu();
-        ForkJoinPool.commonPool().awaitQuiescence(5, TimeUnit.SECONDS);
     }
 }
