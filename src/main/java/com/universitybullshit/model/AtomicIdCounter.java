9b0dd3fe5ea8f1888ac072e3e76018589103ec43
cheremshin <sergey.cheremshin@gmail.com>
2023-04-07 23:42:33 +0700

    feat: Counter field final now

--- src/main/java/com/universitybullshit/model/AtomicIdCounter.java
+++ src/main/java/com/universitybullshit/model/AtomicIdCounter.java
@@ -3,7 +3,7 @@ package com.universitybullshit.model;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class AtomicIdCounter {
-    private static AtomicLong counter = new AtomicLong(0);
+    private static final AtomicLong counter = new AtomicLong(0);
     public static long nextId() {
         return counter.incrementAndGet();
     }
