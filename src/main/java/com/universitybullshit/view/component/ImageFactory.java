c10457e9702920dd985efcedbad4f7106851f9ea
cheremshin <sergey.cheremshin@gmail.com>
2023-04-08 19:21:02 +0700

    chore: Add new images

--- src/main/java/com/universitybullshit/view/component/ImageFactory.java
+++ src/main/java/com/universitybullshit/view/component/ImageFactory.java
@@ -10,7 +10,9 @@ import java.net.URL;
 public class ImageFactory {
 //    private static final String WOODEN_BUILDING_ICON_PATH = "./src/main/resources/woodenBuilding.png";
 //    private static final String CAPITAL_BUILDING_ICON_PATH = "./src/main/resources/capitalBuilding.png";
-    private static final URL IT_DEPRECATED_PATH = ImageFactory.class.getResource("/it_deprecated.png");
+    private static final URL IT_DEPRECATED_PATH = ImageFactory.class.getResource("/img/it_deprecated.png");
+    private static final URL ICON_PATH = ImageFactory.class.getResource("/img/building.png");
+    private static final URL MAIN_MENU_IMAGE_PATH = ImageFactory.class.getResource("/img/main_menu_image.jpg");
 
 //    @Getter
 //    private final Image woodenBuilding;
@@ -19,10 +21,16 @@ public class ImageFactory {
 
     @Getter
     private final Image itDeprecated;
+    @Getter
+    private final Image icon;
+    @Getter
+    private final Image mainMenuImage;
 
     public ImageFactory() {
 //        this.woodenBuilding = new ImageIcon(WOODEN_BUILDING_ICON_PATH).getImage();
 //        this.capitalBuilding = new ImageIcon(CAPITAL_BUILDING_ICON_PATH).getImage();
         this.itDeprecated = new ImageIcon(IT_DEPRECATED_PATH).getImage();
+        this.icon = new ImageIcon(ICON_PATH).getImage();
+        this.mainMenuImage = new ImageIcon(MAIN_MENU_IMAGE_PATH).getImage();
     }
 }
