f524a703d325f81884d915238add9788c092faa7
cheremshin <sergey.cheremshin@gmail.com>
2023-04-08 19:44:15 +0700

    feat: Add rounded borders paint

--- src/main/java/com/universitybullshit/view/util/HintTextField.java
+++ src/main/java/com/universitybullshit/view/util/HintTextField.java
@@ -59,4 +59,27 @@ public class HintTextField extends JTextField {
             }
         });
     }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D)g;
+        g2d.setRenderingHint(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2d.setColor(getBackground());
+        g2d.fillRoundRect(0,0,getWidth() - 1,getHeight() - 1,radius,radius);
+        super.paintComponent(g2d);
+    }
+
+    protected void paintBorder(Graphics g) {
+        Graphics2D g2d = (Graphics2D)g;
+        g2d.setRenderingHint(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+
+        g2d.setColor(getForeground());
+        g2d.drawRoundRect(0,0,getWidth() - 1, getHeight() - 1, radius, radius);
+    }
 }
