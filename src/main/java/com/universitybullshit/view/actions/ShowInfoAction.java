93a54d243b7f4a2c86ccfffe597ea3d5cfc5d929
cheremshin <sergey.cheremshin@gmail.com>
2023-04-27 17:41:48 +0700

    refactor: Remove action to avoid errors

--- src/main/java/com/universitybullshit/view/actions/ShowInfoAction.java
+++ src/main/java/com/universitybullshit/view/actions/ShowInfoAction.java
@@ -1,7 +1,5 @@
 package com.universitybullshit.view.actions;
 
-import com.universitybullshit.view.SimulationWindow;
-
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 
@@ -9,7 +7,5 @@ public class ShowInfoAction extends AbstractAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        SimulationWindow.setShowInfoDialog(!SimulationWindow.isShowInfoDialog());
-        System.out.println(SimulationWindow.isShowInfoDialog());
     }
 }
