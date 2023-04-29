d49f3ff6cceb877281a5f7b43607a7d53f765ab0
cheremshin <sergey.cheremshin@gmail.com>
2023-04-29 17:25:16 +0700

    feat: Now need to init SimulationService with start Habitat.

--- src/main/java/com/universitybullshit/Main.java
+++ src/main/java/com/universitybullshit/Main.java
@@ -1,13 +1,16 @@
 package com.universitybullshit;
 
 import com.universitybullshit.controller.HabitatController;
+import com.universitybullshit.model.Habitat;
+import com.universitybullshit.services.SimulationService;
 import com.universitybullshit.view.WindowManager;
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        HabitatController controller = new HabitatController();
+        SimulationService simulation = new SimulationService(new Habitat(100,100));
+        HabitatController controller = new HabitatController(simulation);
         WindowManager windowManager = new WindowManager(controller);
         SwingUtilities.invokeLater(() -> windowManager.swapPage(windowManager.getCurrentPage()));
     }
