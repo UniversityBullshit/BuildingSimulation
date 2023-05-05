03c5f4752bc23f0a39962c38b96414a36eb5365b
Oleg Asimov <funtazoid@yandex.ru>
2023-05-06 01:35:24 +0700

    feat: implements page label

--- src/main/java/com/universitybullshit/view/pages/PreferencesPage.java
+++ src/main/java/com/universitybullshit/view/pages/PreferencesPage.java
@@ -1,6 +1,7 @@
 package com.universitybullshit.view.pages;
 
 import com.universitybullshit.view.WindowManager;
+import com.universitybullshit.view.fabrics.ComponentFabric;
 import com.universitybullshit.view.menubar.CustomMenuBar;
 
 import javax.swing.*;
@@ -44,20 +45,52 @@ public class PreferencesPage implements IPage {
 
     @Override
     public void initializeComponents() {
+        final String PAGE_LABEL_TEXT = "Preferences";
+        final String WOODEN_LABEL_TEXT = "Wooden Building";
+        final String CAPITAL_LABEL_TEXT = "Capital Building";
+        final String INTERVAL_LABEL_TEXT = "Spawn Interval";
+        final String PROBABILITY_LABEL_TEXT = "Spawn Probability";
+        final String LIFETIME_LABEL_TEXT = "Life Time";
+        final String SPEED_LABEL_TEXT = "Movement Speed";
+        final String SAVE_EXIT_BUTTON_TEXT = "Save and exit";
+        final String RESTORE_BUTTON_NAME = "Restore defaults";
+        final String INTERVAL_FIELDS_HINT = "500-10000 (ms)";
+        final String PROBABILITY_COMBOBOX_HINT = "10-100%";
+        final String LIFETIME_FIELDS_HINT = "1-1000 (s)";
+        final String MOVEMENT_FIELDS_HINT = "1-100 (pts/s)";
+
+        components.put(PAGE_LABEL_NAME, new JLabel(PAGE_LABEL_TEXT));
     }
 
     @Override
     public void setupAppearance() {
-
+        ComponentFabric.setupLabel1((JLabel) components.get(PAGE_LABEL_NAME));
+        ComponentFabric.setupDarkLabel((JLabel) components.get(PAGE_LABEL_NAME));
     }
 
     @Override
     public void draw() {
+        frame.setJMenuBar(new CustomMenuBar(context));
 
+        JPanel rootPanel = new JPanel();
+        rootPanel.setLayout(new BoxLayout(rootPanel, BoxLayout.Y_AXIS));
+
+        rootPanel.add(createPageLabelPanel());
+
+        frame.add(rootPanel);
+//        frame.pack();
+        frame.requestFocusInWindow();
     }
 
     @Override
     public void drawAsDialog(JDialog dialog) {
         // No implementation
+    }
+
+    private JPanel createPageLabelPanel() {
+        JPanel panel = new JPanel();
+        ComponentFabric.setupLightPanel(panel);
+        panel.add(components.get(PAGE_LABEL_NAME));
+        return panel;
     }
 }
