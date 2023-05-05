ad300fb9432bbff76185085a46e98675095ff678
cheremshin <sergey.cheremshin@gmail.com>
2023-05-05 23:39:45 +0700

    feat: Add ability to showDialog when action performed

--- src/main/java/com/universitybullshit/view/actions/StopKeyAction.java
+++ src/main/java/com/universitybullshit/view/actions/StopKeyAction.java
@@ -1,22 +1,27 @@
 package com.universitybullshit.view.actions;
 
 import com.universitybullshit.controller.HabitatController;
+import com.universitybullshit.view.WindowManager;
 import com.universitybullshit.view.components.Area;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 
 public class StopKeyAction extends AbstractAction {
-    private final HabitatController controller;
     private final Area area;
+    private final WindowManager context;
 
-    public StopKeyAction(HabitatController controller, Area area) {
-        this.controller = controller;
+    public StopKeyAction(Area area, WindowManager context) {
         this.area = area;
+        this.context = context;
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-        this.controller.stopSimulation();
+        this.context.getController().stopSimulation();
         this.area.setAreaUpdating(false);
+
+        if (this.area.isShowInfo()) {
+            this.context.showDialog(WindowManager.getINFORMATION_PAGE());
+        }
     }
 }
