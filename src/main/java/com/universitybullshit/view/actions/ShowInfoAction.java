94a6a9c16226fb741354569e9363338cd37dfce0
cheremshin <sergey.cheremshin@gmail.com>
2023-05-05 23:37:50 +0700

    feat: Complete showInfo action

--- src/main/java/com/universitybullshit/view/actions/ShowInfoAction.java
+++ src/main/java/com/universitybullshit/view/actions/ShowInfoAction.java
@@ -1,11 +1,18 @@
 package com.universitybullshit.view.actions;
 
+import com.universitybullshit.view.components.Area;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 
 public class ShowInfoAction extends AbstractAction {
+    private final Area area;
+    public ShowInfoAction(Area area) {
+        this.area = area;
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        area.setShowInfo(!area.isShowInfo());
     }
 }
