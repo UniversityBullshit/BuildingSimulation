59f646d8f0105d4997bcca0181369816920be061
cheremshin <sergey.cheremshin@gmail.com>
2023-05-06 22:26:25 +0700

    feat: Now have setters

--- src/main/java/com/universitybullshit/model/util/BuildingDto.java
+++ src/main/java/com/universitybullshit/model/util/BuildingDto.java
@@ -2,19 +2,26 @@ package com.universitybullshit.model.util;
 
 import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
 
 @Data
 public class BuildingDto {
     @Getter
+    @Setter
     private static long woodenBuildingInterval = 2000;
     @Getter
+    @Setter
     private static double woodenBuildingProbability = 0.3;
     @Getter
+    @Setter
     private static long woodenBuildingLifeTime = 30000;
     @Getter
+    @Setter
     private static long capitalBuildingInterval = 3000;
     @Getter
+    @Setter
     private static double capitalBuildingProbability = 0.7;
     @Getter
+    @Setter
     private static long capitalBuildingLifeTime = 90000;
 }
