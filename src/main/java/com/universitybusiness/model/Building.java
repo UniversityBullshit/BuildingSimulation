609a5d47a62741aba000e2b2767408dc26449f70
Sergei Cheremisin <sergey.cheremshin@gmail.com>
2024-03-22 21:26:29 +0700

    doc: Add comments to fields

--- src/main/java/com/universitybusiness/model/Building.java
+++ src/main/java/com/universitybusiness/model/Building.java
@@ -3,20 +3,47 @@ import lombok.Getter;
 import lombok.Setter;
 
 public abstract class Building {
+    /**
+     * Object id
+     */
     @Getter
     private long id;
+
+    /**
+     * Spawn interval
+     */
     @Getter
     private static long interval;
+
+    /**
+     * Spawn chance
+     */
     @Getter
     private static double probability;
+
+    /**
+     * X coordinate of object
+     */
     @Getter
     @Setter
     private int x;
+
+    /**
+     * Y coordinate of object
+     */
     @Getter
     @Setter
     private int y;
+
+    /**
+     * Creation time
+     */
     @Getter
     private long spawnTime;
+
+    /**
+     * Member that stores time after which object is being deleted
+     */
     @Getter
     private static long lifeTime;
 }
