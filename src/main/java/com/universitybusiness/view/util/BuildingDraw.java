5a5c1d0df65b0c51605fb7d78fdefff22255a8c3
Sergei Cheremisin <sergey.cheremshin@gmail.com>
2024-03-22 22:41:06 +0700

    merge: Complete merge "disable-buttons" branch into develop

--- src/main/java/com/universitybusiness/view/util/BuildingDraw.java
+++ src/main/java/com/universitybusiness/view/util/BuildingDraw.java
@@ -8,11 +8,7 @@ import java.awt.*;
 @Data
 public class BuildingDraw {
     @Getter
-    private static final Color woodenBuildingColor = new Color(17, 102, 28);
+    private static final Integer woodenBuildingSide = 30;
     @Getter
-    private static final Color capitalBuildingColor = new Color(143, 30, 242);
-    @Getter
-    private static final Integer woodenBuildingSide = 3;
-    @Getter
-    private static final Integer capitalBuildingSide = 4;
+    private static final Integer capitalBuildingSide = 40;
 }
