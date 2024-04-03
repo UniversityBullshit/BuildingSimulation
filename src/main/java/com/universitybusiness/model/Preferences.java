7e432a01a7dd4221a879ac4d6bebd6e4590f3ff4
Sergei Cheremisin <sergey.cheremshin@gmail.com>
2024-04-03 19:43:05 +0700

    feat: Add building speed

--- src/main/java/com/universitybusiness/model/Preferences.java
+++ src/main/java/com/universitybusiness/model/Preferences.java
@@ -7,9 +7,11 @@ public class Preferences {
     private static final long DEFAULT_WOODEN_BUILDING_INTERVAL = 2000;
     private static final double DEFAULT_WOODEN_BUILDING_PROBABILITY = 0.7;
     private static final long DEFAULT_WOODEN_BUILDING_LIFE_TIME = 30000;
+    private static final long DEFAULT_WOODEN_BUILDING_SPEED = 5;
     private static final long DEFAULT_CAPITAL_BUILDING_INTERVAL = 3000;
     private static final double DEFAULT_CAPITAL_BUILDING_PROBABILITY = 0.3;
     private static final long DEFAULT_CAPITAL_BUILDING_LIFE_TIME = 90000;
+    private static final long DEFAULT_CAPITAL_BUILDING_SPEED = 5;
 
     @Getter
     @Setter
@@ -22,6 +24,9 @@ public class Preferences {
     private static long woodenBuildingLifeTime = DEFAULT_WOODEN_BUILDING_LIFE_TIME;
     @Getter
     @Setter
+    private static long woodenBuildingSpeed = DEFAULT_WOODEN_BUILDING_SPEED;
+    @Getter
+    @Setter
     private static long capitalBuildingInterval = DEFAULT_CAPITAL_BUILDING_INTERVAL;
     @Getter
     @Setter
@@ -29,13 +34,18 @@ public class Preferences {
     @Getter
     @Setter
     private static long capitalBuildingLifeTime = DEFAULT_CAPITAL_BUILDING_LIFE_TIME;
+    @Getter
+    @Setter
+    private static long capitalBuildingSpeed = DEFAULT_CAPITAL_BUILDING_SPEED;
 
     public static void restoreDefaults() {
         woodenBuildingInterval = DEFAULT_WOODEN_BUILDING_INTERVAL;
         woodenBuildingProbability = DEFAULT_WOODEN_BUILDING_PROBABILITY;
         woodenBuildingLifeTime = DEFAULT_WOODEN_BUILDING_LIFE_TIME;
+        woodenBuildingSpeed = DEFAULT_WOODEN_BUILDING_SPEED;
         capitalBuildingInterval = DEFAULT_CAPITAL_BUILDING_INTERVAL;
         capitalBuildingProbability = DEFAULT_CAPITAL_BUILDING_PROBABILITY;
         capitalBuildingLifeTime = DEFAULT_CAPITAL_BUILDING_LIFE_TIME;
+        capitalBuildingSpeed = DEFAULT_CAPITAL_BUILDING_SPEED;
     }
 }
