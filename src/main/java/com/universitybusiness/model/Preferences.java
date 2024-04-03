08c0ec187d810f8687252f0e8afca7efcbf9c519
Sergei Cheremisin <sergey.cheremshin@gmail.com>
2024-04-03 18:46:25 +0700

    refactor: Change BuildingData to Preferences class

---
+++ src/main/java/com/universitybusiness/model/Preferences.java
@@ -0,0 +1,41 @@
+package com.universitybusiness.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Preferences {
+    private static final long DEFAULT_WOODEN_BUILDING_INTERVAL = 2000;
+    private static final double DEFAULT_WOODEN_BUILDING_PROBABILITY = 0.7;
+    private static final long DEFAULT_WOODEN_BUILDING_LIFE_TIME = 30000;
+    private static final long DEFAULT_CAPITAL_BUILDING_INTERVAL = 3000;
+    private static final double DEFAULT_CAPITAL_BUILDING_PROBABILITY = 0.3;
+    private static final long DEFAULT_CAPITAL_BUILDING_LIFE_TIME = 90000;
+
+    @Getter
+    @Setter
+    private static long woodenBuildingInterval = DEFAULT_WOODEN_BUILDING_INTERVAL;
+    @Getter
+    @Setter
+    private static double woodenBuildingProbability = DEFAULT_WOODEN_BUILDING_PROBABILITY;
+    @Getter
+    @Setter
+    private static long woodenBuildingLifeTime = DEFAULT_WOODEN_BUILDING_LIFE_TIME;
+    @Getter
+    @Setter
+    private static long capitalBuildingInterval = DEFAULT_CAPITAL_BUILDING_INTERVAL;
+    @Getter
+    @Setter
+    private static double capitalBuildingProbability = DEFAULT_CAPITAL_BUILDING_PROBABILITY;
+    @Getter
+    @Setter
+    private static long capitalBuildingLifeTime = DEFAULT_CAPITAL_BUILDING_LIFE_TIME;
+
+    public static void restoreDefaults() {
+        woodenBuildingInterval = DEFAULT_WOODEN_BUILDING_INTERVAL;
+        woodenBuildingProbability = DEFAULT_WOODEN_BUILDING_PROBABILITY;
+        woodenBuildingLifeTime = DEFAULT_WOODEN_BUILDING_LIFE_TIME;
+        capitalBuildingInterval = DEFAULT_CAPITAL_BUILDING_INTERVAL;
+        capitalBuildingProbability = DEFAULT_CAPITAL_BUILDING_PROBABILITY;
+        capitalBuildingLifeTime = DEFAULT_CAPITAL_BUILDING_LIFE_TIME;
+    }
+}
