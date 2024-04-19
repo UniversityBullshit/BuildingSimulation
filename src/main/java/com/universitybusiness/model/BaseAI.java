3b5955c3752f4181c81d0b704d42311540fa5bc4
cheremshin <sergey.cheremshin@gmail.com>
2024-04-19 19:00:48 +0700

    feat: Abstract class that defines movement ability

---
+++ src/main/java/com/universitybusiness/model/BaseAI.java
@@ -0,0 +1,45 @@
+package com.universitybusiness.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.awt.*;
+
+public abstract class BaseAI implements Runnable {
+    protected long speed;
+    protected long multiplier = 100000;
+    protected boolean isMoving = true;
+    protected Point finishPoint;
+    protected final long TIMER_TICK_VALUE = 200;
+
+    public BaseAI(Point finishPoint) {
+        this.finishPoint = finishPoint;
+    }
+
+    protected abstract void move();
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                synchronized (this) {
+                    while (!isMoving) {
+                        this.wait();
+                    }
+
+                    move();
+                }
+            }
+
+            Thread.sleep(TIMER_TICK_VALUE);
+        } catch (Exception ignored) {}
+    }
+
+    public void stopMoving() {
+        isMoving = false;
+    }
+
+    public void resumeMoving() {
+        isMoving = true;
+    }
+}
